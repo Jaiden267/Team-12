68e7551769e1f7c37c7afc112a95033faf534e36
Amaan-Aston <240200938@aston.ac.uk>
2025-11-27 19:18:27 +0000

    Connected database
    Created the database and connected it just need to change every file to .php and make sure that we host the website to the server

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username   = "cs2team12";       
+$password   = "syAr3YckECMRCXZsK1wKPAJNh"; 
+$database   = "cs2team12_db";    
+
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
