7386fed0341b6b32cf277d2a498dd1151d2f469a
Jaiden267 <240186506@aston.ac.uk>
2026-03-22 20:50:27 +0000

    Updated Header and Footer
    Changed the header and footers to different pages and fixed a naming issue

--- aboutus.php
+++ aboutus.php
@@ -82,12 +82,12 @@ require_once 'db_connect.php';
               <div class="mega-col">
                 <h4>Highlights</h4>
                 <a href="womensshirts.php">New in Women</a>
-                <a href="womensknitwear.php">Bestseller</a>
+                <a href="womenactivewear.php">Bestseller</a>
                 
               </div>
               <div class="mega-col">
                 <h4>Activewear</h4>
-                <a href="womanactivewear.php">All Activewear</a>
+                <a href="womenactivewear.php">All Activewear</a>
               </div>
               <div class="mega-col">
                 <h4>Clothing</h4>
@@ -109,7 +109,7 @@ require_once 'db_connect.php';
               <div class="mega-col">
                 <h4>Kids</h4>
                 <a href="kidstshirts.php">T-Shirts</a>
-                <a href="kidsclothing.php">Clothing</a>
+                <a href="kidstshirts.php">Clothing</a>
               </div>
             </div>
           </li>
@@ -189,7 +189,7 @@ require_once 'db_connect.php';
       <div>
         <h5>Support</h5>
         <a href="delivery.php">Delivery</a>
-        <a href="returns.php">Returns</a>
+        <a href="contact.php">Let Us Know How We Did</a>
         <a href="contact.php">Contact Us</a>
       </div>
       <div>
@@ -211,5 +211,6 @@ require_once 'db_connect.php';
     </div>
 </footer>
   <script src="app.js"></script>
+  <script src="//code.tidio.co/t2metx8c6fo4wq7w8lvxrczj0m32nwmk.js" async></script>
 </body>
 </html>
