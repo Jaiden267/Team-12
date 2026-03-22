f0979635304c5038a20767dc8257c946f82608a2
Jaiden267 <240186506@aston.ac.uk>
2026-03-22 20:51:54 +0000

    Updated Header and Footer
    Changed the header and footers to different pages and fixed a naming issue

--- delivery.php
+++ delivery.php
@@ -31,7 +31,7 @@ require_once 'db_connect.php';
   </div>
 </div>
 
-<header class="site-header">
+ <header class="site-header">
     <div class="container header-inner">
       <a href="index.php" class="brand" aria-label="Lunare Clothing Home"> 
         <img src="assets/lunare_logo.png" alt="Lunare Clothing logo" class="brand-img">
@@ -70,12 +70,12 @@ require_once 'db_connect.php';
               <div class="mega-col">
                 <h4>Highlights</h4>
                 <a href="womensshirts.php">New in Women</a>
-                <a href="womensknitwear.php">Bestseller</a>
+                <a href="womenactivewear.php">Bestseller</a>
                 
               </div>
               <div class="mega-col">
                 <h4>Activewear</h4>
-                <a href="womanactivewear.php">All Activewear</a>
+                <a href="womenactivewear.php">All Activewear</a>
               </div>
               <div class="mega-col">
                 <h4>Clothing</h4>
@@ -97,7 +97,7 @@ require_once 'db_connect.php';
               <div class="mega-col">
                 <h4>Kids</h4>
                 <a href="kidstshirts.php">T-Shirts</a>
-                <a href="kidsclothing.php">Clothing</a>
+                <a href="kidstshirts.php">Clothing</a>
               </div>
             </div>
           </li>
@@ -214,7 +214,7 @@ require_once 'db_connect.php';
       <div>
         <h5>Support</h5>
         <a href="delivery.php">Delivery</a>
-        <a href="returns.php">Returns</a>
+        <a href="contact.php">Let Us Know How We Did</a>
         <a href="contact.php">Contact Us</a>
       </div>
       <div>
@@ -245,6 +245,6 @@ document.querySelectorAll('.faq-question').forEach(btn => {
   });
 });
 </script>
-
+<script src="//code.tidio.co/t2metx8c6fo4wq7w8lvxrczj0m32nwmk.js" async></script>
 </body>
 </html>
